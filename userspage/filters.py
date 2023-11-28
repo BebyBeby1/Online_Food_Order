1bd7ca70f088df0497508da83d44229042518e60
BebyBeby1 <152272422+BebyBeby1@users.noreply.github.com>
2023-11-28 17:39:36 +0545

    Add files via upload

---
+++ userspage/filters.py
@@ -0,0 +1,14 @@
+import django_filters
+from demo_app.models import *
+from django_filters import CharFilter
+
+
+class FoodFilter(django_filters.FilterSet):
+	foodname_contains=CharFilter(field_name='food_name',lookup_expr='icontains')
+
+	class Meta:
+		model=Food
+		fields=''
+		exclude=['food_price','food_description','Food_image','category','created_by']
+
+
